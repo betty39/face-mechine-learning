932b84badc6e8f3305811ecb5881f993610f285f
何怡家 <heyijia@betty.local>
2018-10-27 23:15:44 +0800

    change main import flask api && generate matplotlib img分析图片

--- knn_analysis.py
+++ knn_analysis.py
@@ -3,6 +3,7 @@ import knn_kdtree1
 import os
 import operator
 from numpy import *
+import pylab as pl
 
 def pcaAndFaceFindAnalysis(train_data, train_labels, test_data, test_labels, lowDimen, knn):
 	data_train_new,data_mean,V = data_conversion1.pca(train_data, lowDimen)
@@ -24,13 +25,24 @@ def pcaAndFaceFindAnalysis(train_data, train_labels, test_data, test_labels, low
 path = '/Users/heyijia/master/机器学习/人脸识别/att_faces'
 # 获取样本图片原始数据
 print('analysis att_faces')
-train_data, train_labels, test_data, test_labels = data_conversion1.loadDataSetAnalysis(path, 7)
-accuracy = pcaAndFaceFindAnalysis(train_data, train_labels, test_data, test_labels, 30, 4)
+train_data, train_labels, test_data, test_labels = data_conversion1.loadDataSetAnalysis(path, 8)
+accuracy = pcaAndFaceFindAnalysis(train_data, train_labels, test_data, test_labels, 50, 4)
 print('The classify accuracy is: %.2f%%'%(accuracy * 100))
+
+# 画图
+x = []  # 横轴的数据
+y = []
+index = 10
+while (index < 90):
+	y.append(pcaAndFaceFindAnalysis(train_data, train_labels, test_data, test_labels, index, 6))
+	x.append(index)
+	index += 10
+pl.plot(x, y)  # 调用pylab的plot函数绘制曲线
+pl.show()  # 显示绘制出的图
 
 path = '/Users/heyijia/master/机器学习/人脸识别/jaffe'
 # 获取样本图片原始数据
 print('analysis jaffe')
-train_data, train_labels, test_data, test_labels = data_conversion1.loadDataJaffeAnalysis(path, 13)
+train_data, train_labels, test_data, test_labels = data_conversion1.loadDataJaffeAnalysis(path, 10)
 accuracy = pcaAndFaceFindAnalysis(train_data, train_labels, test_data, test_labels, 30, 4)
 print('The classify accuracy is: %.2f%%'%(accuracy * 100))
--- main.py
+++ main.py
@@ -3,16 +3,76 @@ import knn_kdtree1
 import os
 import operator
 from numpy import *
+import uuid
+import platform
+from flask import Flask, abort, request, jsonify
 
-path = '/Users/heyijia/master/机器学习/人脸识别/jaffe'
-# 获取样本图片原始数据
-train_data, train_labels = data_conversion1.loadDataSet(path)
-data_train_new,data_mean,V = data_conversion1.pca(train_data, 30)
-test_path = '/Users/heyijia/master/机器学习/人脸识别/test_face' + '/KA.AN1.39.tiff'
-test_face = data_conversion1.img2vector(test_path)
-num_test = test_face.shape[0]
-temp_face = test_face - tile(data_mean,(num_test,1))
-data_test_new = temp_face*V # 得到测试脸在特征向量下的数据
-data_test_new = array(data_test_new)
-outputLabel = knn_kdtree1.findSimilarLable(data_train_new, train_labels, data_test_new[0,:], 6)
-print('预测结果:' + str(outputLabel))
+if platform.system() == "Windows":
+    slash = '\\'
+else:
+    platform.system()=="Linux"
+    slash = '/'
+
+UPLOAD_FOLDER = 'upload'
+ALLOW_EXTENSIONS = set(['jpg', 'png', 'pgm', 'tiff'])
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+#判断文件夹是否存在，如果不存在则创建
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+else:
+    pass
+# 判断文件后缀是否在列表中
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1] in ALLOW_EXTENSIONS
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/knn-find-face', methods=['POST'])
+def knn_findFace():
+	'''
+	post 传输接受参数例子
+	if not request.json or 'id' not in request.json or 'info' not in request.json:
+        abort(400)
+    task = {
+        'id': request.json['id'],
+        'info': request.json['info']
+    }
+	'''
+	if not request.files or 'file' not in request.files:
+		return buildResponse({}, 400, 'no file upload')
+	file_path = ''
+	#获取post过来的文件名称，从name=file参数中获取
+	file = request.files['file']
+	if file and allowed_file(file.filename):
+		filename = file.filename
+		file_name = str(uuid.uuid4()) + '.' + filename.rsplit('.', 1)[1]
+		#file.save(os.path.join(app.config['UPLOAD_FOLDER']), file_name)
+		file.save(app.config['UPLOAD_FOLDER'] + slash + file_name)
+		file_path = UPLOAD_FOLDER + slash + file_name
+		print(file_path)
+	if file_path == '':
+		return buildResponse({}, 400, 'no file upload')
+
+	path = '/Users/heyijia/master/机器学习/人脸识别/jaffe'
+	# 获取样本图片原始数据
+	train_data, train_labels = data_conversion1.loadDataSet(path)
+	data_train_new,data_mean,V = data_conversion1.pca(train_data, 30)
+	test_path = file_path
+	test_face = data_conversion1.img2vector(test_path)
+	num_test = test_face.shape[0]
+	temp_face = test_face - tile(data_mean,(num_test,1))
+	data_test_new = temp_face*V # 得到测试脸在特征向量下的数据
+	data_test_new = array(data_test_new)
+	outputLabel = knn_kdtree1.findSimilarLable(data_train_new, train_labels, data_test_new[0,:], 6)
+	return buildResponse({'facePath': outputLabel})
+
+def buildResponse(data, code = 0, message = 'success'):
+	return jsonify({'code': code, 'data': data,'message': message})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
